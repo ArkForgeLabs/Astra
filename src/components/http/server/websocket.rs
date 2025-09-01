e06bf1ad723553760d80fb14d6a7c77776eef17d
Elham Aryanpur <elhamaryanpur5@gmail.com>
2025-09-01 23:42:19 +0300

    refactor: unnecessary error messages

--- src/components/http/server/websocket.rs
+++ src/components/http/server/websocket.rs
@@ -1,8 +1,45 @@
 use axum::extract::ws::{CloseFrame, Message, Utf8Bytes, WebSocket};
 use bytes::Bytes;
-use mlua::UserData;
+use mlua::{ExternalError, UserData};
 
 pub struct LuaWebSocket(pub WebSocket);
+impl LuaWebSocket {
+    // search for any integer value in the table that falls into the byte array categorization
+    fn is_byte_array(table: &mlua::Table) -> bool {
+        !table
+            .sequence_values::<mlua::Value>()
+            .filter_map(|i| i.ok())
+            .any(|value| {
+                if let Some(int_value) = value.as_integer()
+                    && !(0..=255).contains(&int_value)
+                {
+                    true
+                } else {
+                    true
+                }
+            })
+    }
+
+    fn value_to_bytes(value: &mlua::Value) -> Result<Bytes, mlua::Error> {
+        if let Some(table) = value.as_table() {
+            if Self::is_byte_array(table) {
+                // Treat as byte array
+                Ok(Bytes::from_iter(
+                    table.sequence_values::<u8>().filter_map(|x| x.ok()),
+                ))
+            } else {
+                // Serialize to JSON and convert to bytes
+                let json_string =
+                    serde_json::to_string(&table.clone()).map_err(|e| e.into_lua_err())?;
+                Ok(Bytes::from(json_string))
+            }
+        } else if value.is_string() {
+            Ok(Bytes::from(value.to_string()?))
+        } else {
+            Err(mlua::Error::runtime("type cannot be accepted as bytes"))
+        }
+    }
+}
 impl UserData for LuaWebSocket {
     fn add_methods<M: mlua::UserDataMethods<Self>>(methods: &mut M) {
         methods.add_async_method_mut("recv", |lua, mut this, ()| async move {
@@ -62,50 +99,18 @@ impl UserData for LuaWebSocket {
                 }
             };
 
-            let msg_value = match message.get::<mlua::Value>(2) {
-                Ok(val) => val,
-                Err(e) => {
-                    return Err(mlua::Error::runtime(format!(
-                        "message is not a valid lua value: {e}"
-                    )));
-                }
-            };
+            let value = message.get::<mlua::Value>("message")?;
+            let msg_value = message.get::<mlua::Value>(2)?;
 
             let msg = match msg_type.to_lowercase().as_str() {
-                "text" => match msg_value.to_string() {
-                    Ok(val) => Ok(Message::Text(Utf8Bytes::from(val))),
-                    Err(e) => Err(mlua::Error::runtime(format!(
-                        "could not convert the value to a string: {e}"
-                    ))),
-                },
-                "bytes" => match msg_value.to_string() {
-                    Ok(val) => Ok(Message::Binary(Bytes::from(val))),
-                    Err(e) => Err(mlua::Error::runtime(format!(
-                        "could not convert the value to a string: {e}"
-                    ))),
-                },
-                "ping" => match msg_value.to_string() {
-                    Ok(val) => Ok(Message::Ping(Bytes::from(val))),
-                    Err(e) => Err(mlua::Error::runtime(format!(
-                        "could not convert the value to a string: {e}"
-                    ))),
-                },
-                "pong" => match msg_value.to_string() {
-                    Ok(val) => Ok(Message::Ping(Bytes::from(val))),
-                    Err(e) => Err(mlua::Error::runtime(format!(
-                        "could not convert the value to a string: {e}"
-                    ))),
-                },
+                "text" => Ok(Message::Text(Utf8Bytes::from(msg_value.to_string()?))),
+                "bytes" => Ok(Message::Binary(Self::value_to_bytes(&value)?)),
+                "ping" => Ok(Message::Pong(Self::value_to_bytes(&value)?)),
+                "pong" => Ok(Message::Ping(Self::value_to_bytes(&value)?)),
                 "close" => match msg_value.as_table() {
                     Some(frame) => Ok(Message::Close(Some(CloseFrame {
-                        code: match frame.get::<u16>(1) {
-                            Ok(code) => code,
-                            Err(_) => 1005,
-                        },
-                        reason: match frame.get::<String>(2) {
-                            Ok(reason) => Utf8Bytes::from(reason),
-                            Err(_) => Utf8Bytes::from_static(""),
-                        },
+                        code: frame.get::<u16>(1).unwrap_or(1005),
+                        reason: Utf8Bytes::from(frame.get::<String>(2)?),
                     }))),
                     None => Ok(Message::Close(None)),
                 },
@@ -114,21 +119,17 @@ impl UserData for LuaWebSocket {
 
             match msg {
                 Ok(msg) => match this.0.send(msg).await {
-                    Ok(passed) => Ok(passed),
-                    Err(e) => Err(mlua::Error::runtime(format!(
-                        "message could not be sent: {e}"
-                    ))),
+                    Ok(_) => Ok(()),
+                    Err(e) => Err(e.into_lua_err()),
                 },
-                Err(e) => Err(mlua::Error::runtime(e)),
+                Err(e) => Err(e.into_lua_err()),
             }
         });
 
         methods.add_async_method_mut("send_text", |_, mut this, message: String| async move {
             match this.0.send(Message::Text(Utf8Bytes::from(message))).await {
-                Ok(passed) => Ok(passed),
-                Err(e) => Err(mlua::Error::runtime(format!(
-                    "message could not be sent: {e}"
-                ))),
+                Ok(_) => Ok(()),
+                Err(e) => Err(e.into_lua_err()),
             }
         });
 
@@ -140,8 +141,8 @@ impl UserData for LuaWebSocket {
                     .send(Message::Binary(Bytes::from(bytes.to_string_lossy())))
                     .await
                 {
-                    Ok(passed) => Ok(passed),
-                    Err(e) => Err(mlua::Error::runtime(e)),
+                    Ok(_) => Ok(()),
+                    Err(e) => Err(e.into_lua_err()),
                 }
             },
         );
@@ -152,8 +153,8 @@ impl UserData for LuaWebSocket {
                 .send(Message::Ping(Bytes::from(bytes.to_string_lossy())))
                 .await
             {
-                Ok(passed) => Ok(passed),
-                Err(e) => Err(mlua::Error::runtime(e)),
+                Ok(_) => Ok(()),
+                Err(e) => Err(e.into_lua_err()),
             }
         });
 
@@ -163,8 +164,8 @@ impl UserData for LuaWebSocket {
                 .send(Message::Pong(Bytes::from(bytes.to_string_lossy())))
                 .await
             {
-                Ok(passed) => Ok(passed),
-                Err(e) => Err(mlua::Error::runtime(e)),
+                Ok(_) => Ok(()),
+                Err(e) => Err(e.into_lua_err()),
             }
         });
 
@@ -173,23 +174,15 @@ impl UserData for LuaWebSocket {
             |_, mut this, close_frame: Option<mlua::Table>| async move {
                 let close_frame: Message = match close_frame {
                     Some(frame) => Message::Close(Some(CloseFrame {
-                        code: match frame.get::<u16>(1) {
-                            Ok(code) => code,
-                            Err(_) => 1005,
-                        },
-                        reason: match frame.get::<String>(2) {
-                            Ok(reason) => Utf8Bytes::from(reason),
-                            Err(_) => Utf8Bytes::from_static(""),
-                        },
+                        code: frame.get::<u16>(1).unwrap_or(1005),
+                        reason: Utf8Bytes::from(frame.get::<String>(2)?),
                     })),
                     None => Message::Close(None),
                 };
 
                 match this.0.send(close_frame).await {
-                    Ok(passed) => Ok(passed),
-                    Err(e) => Err(mlua::Error::runtime(format!(
-                        "message could not be sent: {e}"
-                    ))),
+                    Ok(_) => Ok(()),
+                    Err(e) => Err(e.into_lua_err()),
                 }
             },
         );
