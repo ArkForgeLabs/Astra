78b4928285dbc31c8587d43d5d8ac44cfc7ceaf8
Elham Aryanpur <elhamaryanpur5@gmail.com>
2025-11-17 11:11:53 +0300

    feat: added major serde targets

--- astra/lua/serde.lua
+++ astra/lua/serde.lua
@@ -1,21 +1,42 @@
+---@meta
+
 local json = {}
 
 ---Encodes the value into a valid JSON string
 ---@param value any
 ---@return string
 function json.encode(value)
-	---@diagnostic disable-next-line: undefined-global
-	return astra_internal__json_encode(value)
+  ---@diagnostic disable-next-line: undefined-global
+  return astra_internal__json_encode(value)
 end
 
 ---Decodes the JSON string into a valid lua value
 ---@param value string
 ---@return any
 function json.decode(value)
-	---@diagnostic disable-next-line: undefined-global
-	return astra_internal__json_decode(value)
+  ---@diagnostic disable-next-line: undefined-global
+  return astra_internal__json_decode(value)
+end
+
+local yaml = {}
+
+---Encodes the value into a valid YAML string
+---@param value any
+---@return string
+function yaml.encode(value)
+  ---@diagnostic disable-next-line: undefined-global
+  return astra_internal__yaml_encode(value)
+end
+
+---Decodes the YAML string into a valid lua value
+---@param value string
+---@return any
+function yaml.decode(value)
+  ---@diagnostic disable-next-line: undefined-global
+  return astra_internal__yaml_decode(value)
 end
 
 return {
-	json = json
+  json = json,
+  yaml = yaml
 }
--- src/components/astra_serde.rs
+++ src/components/astra_serde.rs
@@ -1,55 +1,86 @@
 use mlua::{ExternalError, LuaSerdeExt};
 
 pub fn register_to_lua(lua: &mlua::Lua) -> mlua::Result<()> {
-    // json
     json_encode(lua)?;
     json_decode(lua)?;
+
+    json5_encode(lua)?;
+    json5_decode(lua)?;
+
+    yaml_encode(lua)?;
+    yaml_decode(lua)?;
+
+    ini_encode(lua)?;
+    ini_decode(lua)?;
+
+    xml_encode(lua)?;
+    xml_decode(lua)?;
+
+    toml_encode(lua)?;
+    toml_decode(lua)?;
 
     Ok(())
 }
 
-fn json_encode(lua: &mlua::Lua) -> mlua::Result<()> {
-    lua.globals().set(
-        "astra_internal__json_encode",
-        lua.create_function(|lua, input: mlua::Value| {
-            // removing functions
-            let input = if let Some(input) = input.as_table() {
-                let new_input = lua.create_table()?;
+fn sanetize_lua_input(lua: &mlua::Lua, input: mlua::Value) -> mlua::Result<mlua::Value> {
+    if let Some(input) = input.as_table() {
+        let new_input = lua.create_table()?;
 
-                for pair in input.pairs::<mlua::Value, mlua::Value>() {
-                    let (key, value) = pair?;
-                    if !value.is_function()
-                        && !value.is_light_userdata()
-                        && !value.is_userdata()
-                        && !value.is_error()
-                        && !value.is_thread()
-                    {
-                        new_input.set(key, value)?;
+        for pair in input.pairs::<mlua::Value, mlua::Value>() {
+            let (key, value) = pair?;
+            if !value.is_function()
+                && !value.is_light_userdata()
+                && !value.is_userdata()
+                && !value.is_error()
+                && !value.is_thread()
+            {
+                new_input.set(key, value)?;
+            }
+        }
+
+        lua.to_value(&new_input)
+    } else {
+        Ok(input)
+    }
+}
+
+#[crabtime::function]
+fn gen_methods(crate_name: String, name: String) {
+    let encode_str = format!("\"astra_internal__{name}_encode\"");
+    let decode_str = format!("\"astra_internal__{name}_decode\"");
+
+    crabtime::output!(
+        fn {{name}}_encode(lua: &mlua::Lua) -> mlua::Result<()> {
+            lua.globals().set(
+                {{encode_str}},
+                lua.create_function(|lua, input: mlua::Value| {
+                    let value =
+                        lua.from_value::<serde_value::Value>(sanetize_lua_input(lua, input)?)?;
+                    match {{crate_name}}::to_string(&value) {
+                        Ok(serialized) => Ok(lua.to_value(&serialized)?),
+                        Err(e) => Err(e.into_lua_err()),
                     }
-                }
+                })?,
+            )
+        }
 
-                lua.to_value(&new_input)?
-            } else {
-                input
-            };
-
-            let json_value = lua.from_value::<serde_json::Value>(input)?;
-            match serde_json::to_string(&json_value) {
-                Ok(serialized) => Ok(lua.to_value(&serialized)?),
-                Err(e) => Err(e.into_lua_err()),
-            }
-        })?,
+        fn {{name}}_decode(lua: &mlua::Lua) -> mlua::Result<()> {
+            lua.globals().set(
+                {{decode_str}},
+                lua.create_function(|lua, input: String| {
+                    match {{crate_name}}::from_str::<serde_value::Value>(&input) {
+                        Ok(deserialized) => lua.to_value(&deserialized),
+                        Err(e) => Err(e.into_lua_err()),
+                    }
+                })?,
+            )
+        }
     )
 }
 
-fn json_decode(lua: &mlua::Lua) -> mlua::Result<()> {
-    lua.globals().set(
-        "astra_internal__json_decode",
-        lua.create_function(|lua, input: String| {
-            match serde_json::from_str::<serde_json::Value>(&input) {
-                Ok(deserialized) => lua.to_value(&deserialized),
-                Err(e) => Err(e.into_lua_err()),
-            }
-        })?,
-    )
-}
+gen_methods!("serde_json", "json");
+gen_methods!("serde_json5", "json5");
+gen_methods!("serde_yaml", "yaml");
+gen_methods!("serde_ini", "ini");
+gen_methods!("serde_xml_rs", "xml");
+gen_methods!("toml", "toml");
